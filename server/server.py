b043f1a85994acdb223c5e17148de4311ea2bb30
FransiscoReadyPermana <fransiscoreadyy@gmail.com>
2022-01-24 17:47:44 +0700

    confusing how to acces directiry on python

--- server/server.py
+++ server/server.py
@@ -5,6 +5,17 @@ app = Flask(__name__)
 
 @app.route('/hallo', methods=['GET']) 
 
+@app.route('/predict', methods=['POST'])
+
+def predict():
+  total_sqft = float(request.form['total_sqft'])
+  location = request.form['location']
+  bhk = int(request.form['bhk'])
+  bath = int(request.form['bath'])
+  response = jsonify({'estimated_price': util.getEstimatedPrice(location, total_sqft, bath, bhk)})
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  return response
+
 def getLocationName():
   response = jsonify({'location': util.getLocationName()})  
 
--- server/util.py
+++ server/util.py
@@ -1,31 +1,47 @@
 import json
 import pickle
+import numpy as np
 
 __location = None
 __dataColumns = None
 __model = None
 
+def getEstimatedPrice(location, sqft, bath, bhk):
+    try:
+        loc_index = __dataColumns.index(location.lower())
+    except:
+        loc_index = -1
+    x = np.zeros(len(__dataColumns))
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
+    if loc_index >= 0:
+        x[loc_index] = 1
+    return round(__model.predict([x])[0],2)
 
 def getLocationName():
-    pass
+    return __location
 
 def loadSavedArtifacts():
     print("Loading saved artifacts...")
     global __location
     global __dataColumns
+    global __model
 
-    with open("./artifacts/columns.json", "r") as f:
-        __dataColumns = json.load(f)
-        data = json.load(f)
+    with open("columns.json", "r") as f:
+        __dataColumns = json.load(f)['data_columns']
         __location = __dataColumns[3:]
-        print("Loaded artfacts: " + str(data))
+        # print("Loaded artfacts: " + str(__dataColumns))
     
-    with open('./artifacts/banglore_home_prices.pickle', 'rb') as f:
+    with open('D:\Ready\Project\house_pred\house-pred-sc-yt\server\Artifacts\Banglore_home_prices.pickle', 'rb') as f:
         __model = pickle.load(f)
-        print("Loaded model: " + str(__model))
+    print("Loaded model: " + str(__model))
 
 
 
 if __name__ == '__main__':
     loadSavedArtifacts()
-    print (getLocationName())
+    # print (getLocationName())
+    
+    print(getEstimatedPrice('1st Phase JP Nagar', 1000, 3, 2))
+    print(getEstimatedPrice('kalhalli', 1000, 3, 2))
